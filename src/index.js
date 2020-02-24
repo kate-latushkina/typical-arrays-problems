093b361f7d89cd5a8c3fdfef7d27337a4f79e066
kate-latushkina <59828320+kate-latushkina@users.noreply.github.com>
2020-02-24 13:33:46 +0200

    feat: add task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let min = 0;
+  for (let i = 0; i < array.length; i++) {
+    min = array[0];
+    if (array.length != 0) {
+      min = Math.min.apply(null, array);
+    }
+    else {
+      min = 0;
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    max = array[0];
+    if (array.length != 0) {
+      max = Math.max.apply(null, array);
+    }
+    else {
+      max = 0;
+    }
+
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let average = 0;
+  let avg = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array.length != 0) {
+      average += array[i] / array.length;
+      avg = average.toFixed(1)
+    }
+    else {
+      average = 0; 
+    }
+  }
+  return avg;
 }
